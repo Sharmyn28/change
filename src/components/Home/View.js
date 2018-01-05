2b77434b90e2e96ef2c7d8f0f941eb66f954c4fb
Sharmyn28 <sharmynrodriguez@gmail.com>
2018-01-05 14:26:38 -0500

    tooltip 3

--- src/components/Home/View.js
+++ src/components/Home/View.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Image } from "react-bootstrap";
+import { Col, Image, OverlayTrigger, Popover } from "react-bootstrap";
 import background from "../../img/background/back400.png";
 
 const Initial = () => {
@@ -12,7 +12,7 @@ const Initial = () => {
 }
 
 const InitalSection = () => {
-    return(
+    return (
         <section className='center'>
             <Col md={6} lg={6} sm={7}>
                 <h3> About Us. </h3>
@@ -22,17 +22,31 @@ const InitalSection = () => {
                     fundamental que desempeña en el desarrollo profesional de las personas. Nuestro enfoque se centra en ayudar a las personas a aprender.</p>
             </Col>
             <Col md={6} lg={6} sm={5}>
-                <Image src={background} responsive/>
+                <Image src={background} responsive />
             </Col>
         </section>
     )
 }
 
-const Circle = ({name}) => {
-    return(
-        <div className='about--story__circle '>
-            <i className={name} ></i>
-        </div>
+const Circle = ({ id, title, info, name }) => {
+    /**
+     * var divStyle = {
+        color: 'white',
+        backgroundImage: 'url(' + imgUrl + ')',
+        WebkitTransition: 'all', // note the capital 'W' here
+        msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+    }
+    */
+
+    const style = {
+        fontSize: '12px'
+    }
+    return (
+        <OverlayTrigger placement='bottom' overlay={<Popover id={id} title={title} className='justify' style={style} > {info} </Popover>} trigger={['hover', 'focus']} >
+            <div className='about--story__circle'>
+                <i className={name} ></i>
+            </div>
+        </OverlayTrigger>
     )
 }
 
@@ -41,22 +55,22 @@ const Story = () => {
         <section>
             <Col lg={4} md={4} sm={4} xs={4} >
                 {/*<p> Capacitación </p>*/}
-                <Circle name="fa fa-wrench"/>
+                <Circle id='1' title='Capacitación' info='Existen en nuestro país muchos docentes que necesitan ser capacitados pero no tienen acceso a talleres o cursos de calidad y que estén a su alcance.' name="fa fa-wrench" />
             </Col>
             <Col lg={4} md={4} sm={4} xs={4}>
                 {/*<p> Diseño Curricular </p>*/}
-                <Circle name='fa fa-pencil-square-o'/>
+                <Circle id='2' title='Diseño Curricular' info='El inglés es una necesidad hoy en día. Muchas veces los programas tradicionales no se alinean a los objetivos de los estudiantes.' name='fa fa-pencil-square-o' />
             </Col>
             <Col lg={4} md={4} sm={4} xs={4}>
                 {/*<p> Asesoría Educativa </p>*/}
-                <Circle name='fa fa-commenting-o'/>
+                <Circle id='3' title='Asesoría Educativa' info='Muchos centros educativos cuentan con tecnología que no saben aprovechar al máximo. Otros, están buscando innovar.' name='fa fa-commenting-o' />
             </Col>
         </section>
     )
 }
 
 const Mission = () => {
-    return(
+    return (
         <section>
             <Col md={6} lg={6} className='justify'>
                 <h4> Nuestra razón de ser. </h4>
